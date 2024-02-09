62b7b48e4642527a4aeca98e44acea874baafa0a
Diele <dybs1520@gmail.com>
2024-02-08 23:07:34 -0300

    criado controller e classe

---
+++ ApiFilme/Controllers/FilmesController.cs
@@ -0,0 +1,28 @@
+﻿using ApiFilme.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFilme.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]//responsavel por inserir os recursos no sistema 
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Equals(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+
+        }
+
+        [HttpGet]//responsavel por ler os recursos do sistema
+        public List<Filme> recuperaFilmes()
+        {
+            return filmes;
+        }
+
+    }
+}
---
+++ ApiFilme/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O titulo é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Genero é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero não pode passar o máximo de 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "A duração é obrigatória")]
+        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
