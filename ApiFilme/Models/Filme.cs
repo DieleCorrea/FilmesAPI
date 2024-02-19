706449862636c7e076729d5c41105241ddca5ece
DieleCorrea <dybs1520@gmail.com>
2024-02-19 11:03:15 -0300

    [ FEAT ] propriedade id

--- ApiFilme/Models/Filme.cs
+++ ApiFilme/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace ApiFilme.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = "O titulo é obrigatório")]
         public string Titulo { get; set; }
 
@@ -14,5 +15,6 @@ namespace ApiFilme.Models
         [Required(ErrorMessage = "A duração é obrigatória")]
         [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
         public int Duracao { get; set; }
+        
     }
 }
