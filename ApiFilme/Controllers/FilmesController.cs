603f680b0ab51dac120d5d93d0bb6e4094beed05
DieleCorrea <dybs1520@gmail.com>
2024-02-19 11:02:36 -0300

    [ FEATURE ] Recuperar filme por Id e paginação
    -Adicionei o metodo que faz essa recuperação pelo Id
    -Implementei a propriedade id
    -Ao recuperar os filmes agora é feita apaginação de forma direta

--- ApiFilme/Controllers/FilmesController.cs
+++ ApiFilme/Controllers/FilmesController.cs
@@ -7,22 +7,30 @@ namespace ApiFilme.Controllers
     [Route("[controller]")]
     public class FilmesController : ControllerBase
     {
-        public static List<Filme> filmes = new List<Filme>();
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost]//responsavel por inserir os recursos no sistema 
         public void AdicionaFilme([FromBody] Filme filme)
         {
-            filmes.Equals(filme);
+            filme.Id = id++;
+            filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
 
         }
 
         [HttpGet]//responsavel por ler os recursos do sistema
-        public List<Filme> recuperaFilmes()
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip=0,
+            [FromQuery] int take=50)
         {
-            return filmes;
+            return filmes.Skip(skip).Take(take);
         }
 
+        [HttpGet("{id}")]//responsavel por ler os recursos do sistema
+        public Filme? RecuperaFilmesPorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id ==  id);
+        }
     }
 }
