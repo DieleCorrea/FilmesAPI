d16c7d0fbb3070be931d0f1c3404eab3458360a2
Diele <dybs1520@gmail.com>
2024-02-29 22:53:24 -0300

    [FEAT] Adicionando cinema
    -Criei um profille pois assim posso mapear os dtos de cinema

---
+++ ApiFilme/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using ApiFilme.Data.Dtos;
+using ApiFilme.Models;
+using AutoMapper;
+
+namespace ApiFilme.Profiles
+{
+    public class CinemaProfile: Profile
+    {
+        public CinemaProfile()
+        {
+                CreateMap<CreateCinemaDto, Cinema>();
+                CreateMap<Cinema, ReadCinemaDto>();
+                CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
