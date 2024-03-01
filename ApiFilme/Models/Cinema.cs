b7a57a41065b55322602f25ce7e591e6a8516af2
Diele <dybs1520@gmail.com>
2024-02-29 22:49:40 -0300

    [FEAT] Adicionando cinema
    -Criei uma model para cinema com ide nome

---
+++ ApiFilme/Models/Cinema.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Models
+{
+    public class Cinema
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O camppo nome é obrigatório")]
+        public string Nome { get; set; }
+    }
+}
