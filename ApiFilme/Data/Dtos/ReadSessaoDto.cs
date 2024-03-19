0939e9257d078bc1a411dbe8ee594d94d1d53715
Diele <dybs1520@gmail.com>
2024-03-19 00:01:21 -0300

    [FEAT] Incluir Sessão

---
+++ ApiFilme/Data/Dtos/ReadSessaoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiFilme.Data.Dtos
+{
+    public class ReadSessaoDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ApiFilme/Profiles/SessaoProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApiFilme.Data.Dtos;
+using ApiFilme.Models;
+using AutoMapper;
+
+namespace ApiFilme.Profiles
+{
+    public class SessaoProfile : Profile
+    {
+        public SessaoProfile()
+        {
+            CreateMap<CreateSessaoDto, Sessao>();
+            CreateMap<Sessao, ReadSessaoDto>();
+        }
+    }
+}
