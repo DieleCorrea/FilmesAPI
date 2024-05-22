55b60dc90d56a0fb498666fa1f3ce89dc9452cda
Diele <dybs1520@gmail.com>
2024-05-22 18:01:59 -0300

    [ FEAT ] Criação profile para fazer o mapeamento

---
+++ ApiFilme/Profiles/ClienteProfile.cs
@@ -0,0 +1,16 @@
+﻿using ApiFilme.Data.Dtos;
+using ApiFilme.Models;
+using AutoMapper;
+
+namespace ApiFilme.Profiles
+{
+    public class ClienteProfile : Profile
+    {
+        public ClienteProfile()
+        {
+            CreateMap<CreateClienteDto, Cliente>();
+            CreateMap<Cliente, ReadClienteDto>();
+            CreateMap<UpdateClienteDto, Cliente>();
+        }
+    }
+}
