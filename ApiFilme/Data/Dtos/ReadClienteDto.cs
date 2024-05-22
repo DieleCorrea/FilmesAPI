c4166383fa95d151648b4a7c1b16c4e9c1f32faa
Diele <dybs1520@gmail.com>
2024-05-22 16:09:10 -0300

    [ FEAT ] criação dos Dtos

---
+++ ApiFilme/Data/Dtos/ReadClienteDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ApiFilme.Data.Dtos
+{
+    public class ReadClienteDto
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Contato { get; set; }
+        public string? Anotacoes { get; set; }
+    }
+}
---
+++ ApiFilme/Data/Dtos/UpdateClienteDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Data.Dtos
+{
+    public class UpdateClienteDto
+    {
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Contato é obrigatorio")]
+        public string Contato { get; set; }
+        public string? Anotacoes { get; set; } = null;
+    }
+}
