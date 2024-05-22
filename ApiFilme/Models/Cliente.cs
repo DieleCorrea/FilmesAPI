09d0d4904177da243d1ada98b63dd057d58d7f9f
Diele <dybs1520@gmail.com>
2024-05-22 15:54:02 -0300

    [ FEAT ] Criação classe cliente

---
+++ ApiFilme/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Required]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Contato é obrigatorio")]
+        public string Contato { get; set; }
+        public  string Anotacoes { get; set; }
+
+        //public virtual ICollection<Agendas> Agendas { get; set; }
+    }
+}
