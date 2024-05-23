8359239e0460514a73332719339215dde225640c
Diele <dybs1520@gmail.com>
2024-05-23 15:42:24 -0300

    [ FEAT ] Criação de migrations de serviço e cliente

--- ApiFilme/Migrations/20240523165436_tabelaclienteeservico.Designer.cs
+++ ApiFilme/Migrations/20240523165436_tabelaclienteeservico.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiFilme.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiFilme.Migrations
 {
     [DbContext(typeof(FilmeContext))]
-    partial class FilmeContextModelSnapshot : ModelSnapshot
+    [Migration("20240523165436_tabelaclienteeservico")]
+    partial class tabelaclienteeservico
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
