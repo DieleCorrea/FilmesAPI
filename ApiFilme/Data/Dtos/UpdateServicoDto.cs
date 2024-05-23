0494bb314d27cc132756a21271fe2bd9441fe0c2
Diele <dybs1520@gmail.com>
2024-05-23 09:34:47 -0300

    [ FEAT ] Ciação de Dto de serviço

---
+++ ApiFilme/Data/Dtos/UpdateServicoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFilme.Data.Dtos
+{
+    public class UpdateServicoDto
+    {
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O TempoDeExecucao é obrigatório")]
+        public TimeSpan TempoDeExecucao { get; set; }
+        [Required(ErrorMessage = "O Valor é obrigatório")]
+        public decimal Valor { get; set; }
+    }
+}
